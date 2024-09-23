93af5359f52b5af3e645525a504cd5cf736da026
simon-dock <simck11769@gmail.com>
2024-09-23 20:30:30 +0900

    mtg apiを導入

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,5 @@ import { hello } from './example-module';
 
 console.log(hello());
 console.log('hai');
+const mtg = require('mtgsdk')
+const response = UrlFetchApp.fetch("https://qiita.com/api/v2/users/Qiitaのユーザ名/items")
